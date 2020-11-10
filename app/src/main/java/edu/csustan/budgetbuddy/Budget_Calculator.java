2daa6178bd7bd4d07c8f51a763730f20fa92c2e9
Chris Gallo <cgallo@csustan.edu>
2020-11-10 11:08:58 -0800

    fixed empty/blank input bug, still needs decimal validation

--- app/src/main/java/edu/csustan/budgetbuddy/Budget_Calculator.java
+++ app/src/main/java/edu/csustan/budgetbuddy/Budget_Calculator.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Budget_Calculator extends AppCompatActivity {
 
@@ -35,29 +36,33 @@ public class Budget_Calculator extends AppCompatActivity {
         calculateRemainingBalance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int numAmountBalance = Integer.parseInt(amountBalance.getText().toString());
-                int numAverageExpenses = Integer.parseInt(averageExpenses.getText().toString());
-                int numIncome = Integer.parseInt(income.getText().toString());
-                int numRemainingBalance = numAmountBalance - numAverageExpenses + numIncome;
-                int numTotalDisposable = numIncome - numAverageExpenses;
+                String numAmountBalance = amountBalance.getText().toString();
+                String numAverageExpenses = averageExpenses.getText().toString();
+                String numIncome = income.getText().toString();
 
-                // check for null values after clicking button
-
-                if(numRemainingBalance >= 0) {
-                    remainingBalance.setText("Remaining Balance: $" + String.valueOf(numRemainingBalance));
+                if(numAmountBalance.matches("") || numAverageExpenses.matches("") || numIncome.matches("")){
+                    Toast.makeText(getApplicationContext(), "Fill in empty fields",Toast.LENGTH_SHORT).show();
                 }
                 else {
-                    remainingBalance.setText("Remaining Balance: -$" + String.valueOf(Math.abs(numRemainingBalance)));
-                }
-                if(numTotalDisposable >= 0) {
-                    totalDisposable.setText("Total Disposable: $" + String.valueOf(numTotalDisposable));
-                    balance.setText("Good Job!");
-                    balance.setTextColor(Color.parseColor("#008000"));
-                }
-                else {
-                    totalDisposable.setText("Total Disposable: -$" + String.valueOf(Math.abs(numTotalDisposable)));
-                    balance.setText("You're spending too much");
-                    balance.setTextColor(Color.parseColor("#DC143C"));
+                    int numRemainingBalance = Integer.parseInt(numAmountBalance) - Integer.parseInt(numAverageExpenses) + Integer.parseInt(numIncome);
+                    int numTotalDisposable = Integer.parseInt(numIncome) - Integer.parseInt(numAverageExpenses);
+                    
+                    if(numRemainingBalance >= 0) {
+                        remainingBalance.setText("Remaining Balance: $" + String.valueOf(numRemainingBalance));
+                    }
+                    else {
+                        remainingBalance.setText("Remaining Balance: -$" + String.valueOf(Math.abs(numRemainingBalance)));
+                    }
+                    if(numTotalDisposable >= 0) {
+                        totalDisposable.setText("Total Disposable: $" + String.valueOf(numTotalDisposable));
+                        balance.setText("Good Job!");
+                        balance.setTextColor(Color.parseColor("#008000"));
+                    }
+                    else {
+                        totalDisposable.setText("Total Disposable: -$" + String.valueOf(Math.abs(numTotalDisposable)));
+                        balance.setText("You're spending too much");
+                        balance.setTextColor(Color.parseColor("#DC143C"));
+                    }
                 }
             }
         });
