01135b39ba8c0d9d56c3222661b696b432c7706e
Natasha Garcia <56941579+ngarcia34@users.noreply.github.com>
2020-11-09 18:18:02 -0800

    firstBuildingCredit

---
+++ app/src/main/java/edu/csustan/budgetbuddy/Credit.java
@@ -0,0 +1,32 @@
+package edu.csustan.budgetbuddy;
+
+public class Credit {
+
+    private final String bankName;
+    private final String bankDescription;
+    private final String bestFor;
+    private final String applicationLink;
+
+    public Credit(String bankName, String bankDescription, String bestFor, String applicationLink) {
+        this.bankName = bankName;
+        this.bankDescription = bankDescription;
+        this.bestFor = bestFor;
+        this.applicationLink = applicationLink;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public String getBankDescription() {
+        return bankDescription;
+    }
+
+    public String getBestFor() {
+        return bestFor;
+    }
+
+    public String getApplicationLink() {
+        return applicationLink;
+    }
+}
--- app/src/main/java/edu/csustan/budgetbuddy/MainActivity.java
+++ app/src/main/java/edu/csustan/budgetbuddy/MainActivity.java
@@ -1,8 +1,25 @@
 package edu.csustan.budgetbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.csustan.budgetbuddy.adapters.CreditAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +27,81 @@ public class MainActivity extends AppCompatActivity {
     //adding comment - Steph
     //added a comment - Jorge
 
+
+    //code for Building Credit function 
+    private RecyclerView mRecyclerView;
+    private List<Object> viewItems = new ArrayList<>();
+
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new CreditAdapter(this, viewItems);
+        mRecyclerView.setAdapter(mAdapter);
+
+        addItemsFromJSON();
+
+    }
+
+    private void addItemsFromJSON() {
+        try {
+
+            String jsonDataString = readJSONDataFromFile();
+            JSONArray jsonArray = new JSONArray(jsonDataString);
+
+            for (int i=0; i<jsonArray.length(); ++i) {
+
+                JSONObject itemObj = jsonArray.getJSONObject(i);
+
+                String bankName = itemObj.getString("bank_Name");
+                String bankDescription = itemObj.getString("bank_Description");
+                String bestFor = itemObj.getString("best_For");
+                String applicationLink = itemObj.getString("application_Link");
+
+                Credit credit = new Credit(bankName, bankDescription, bestFor,applicationLink);
+                viewItems.add(credit);
+
+            }
+        } catch (JSONException | IOException e) {
+            Log.d(TAG, "addItemsFromJSON: ", e);
+
+        }
+    }
+
+    private String readJSONDataFromFile() throws IOException{
+
+        InputStream inputStream = null;
+        StringBuilder builder = new StringBuilder();
+
+        try {
+
+            String jsonString = null;
+            inputStream = getResources().openRawResource(R.raw.buildingcredit);
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(inputStream, "UTF-8"));
+
+            while ((jsonString = bufferedReader.readLine()) != null) {
+                builder.append(jsonString);
+            }
+
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return new String(builder);
     }
 }
+
+
